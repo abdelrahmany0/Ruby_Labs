02b87b149385f1421c56bf99671bce08814763f3
abdelrahmany0 <abdelrhmanatta01@gmail.com>
2021-05-07 17:27:27 +0200

    fixing multiplication rule

--- lab1.rb
+++ lab1.rb
@@ -1,42 +1,44 @@
 puts "Lab 1 running"
 
 
-class ComplexNumber 
-    @@add_opertations = 0
-    @@multi_opertations = 0
-    attr_accessor :comp
-    attr_accessor :real
-    attr_accessor :imag
+class ComplexNumber
+    @@operations = {'add_operations'=>0, 'multi_opertations'=>0, 'bulk_add'=>0, 'bulk_multi'=>0} 
+    attr_accessor :comp, :real, :imag
     # attr_accessor :opertations
     def initialize(r,i)
-        @comp = Complex(r,i)
+        @comp = Complex(r, i)
         @real = r
         @imag = i
     end
 
     def +(obj)
-        @@add_opertations+=1
-        return ComplexNumber.new(self.real + obj.real, self.imag + obj.imag)
+        @@operations['add_operations']+=1
+        ComplexNumber.new(self.real + obj.real, self.imag + obj.imag)
     end
 
     def *(obj)
-        @@multi_opertations+=1
-        return ComplexNumber.new(self.real * obj.real, self.imag * obj.imag)
+        @@operations['multi_opertations']+=1
+        ComplexNumber.new(
+            (self.real * obj.real) - (self.imag * obj.imag), 
+            (self.real * obj.imag) + (self.imag * obj.real)
+        )
     end
 
     def bulk_add(arr)
-        @@add_opertations+=1
-        return arr.map(&:comp).sum
+        @@operations['bulk_add']+=1
+        arr.map(&:comp).sum
     end
 
     def bulk_mlultiply(arr)
-        @@multi_opertations+=1
-        return arr.map(&:comp).inject(:*)
+        @@operations['bulk_multi']+=1
+        arr.map(&:comp).inject(:*)
     end
 
     def get_stats()
-        puts "add operations: " + @@add_opertations.to_s
-        puts "multiply operations: " + @@multi_opertations.to_s
+        puts "add operations: " + @@operations['add_operations'].to_s
+        puts "multiply operations: " + @@operations['multi_opertations'].to_s
+        puts "bulk_add operations: " + @@operations['bulk_add'].to_s
+        puts "bulk_multi operations: " + @@operations['bulk_multi'].to_s
     end
 end
 
@@ -48,19 +50,24 @@ c2 = ComplexNumber.new(1, -2)
 puts("ADDITION")
 c3 = c1 + c2
 puts c3.comp
+puts ""
 
 puts("MULTIPLICATION")
 c4 = c1 * c2
 puts c4.comp
+puts ""
 
 multi = [c1, c2]
 
 puts "bulk add"
 puts c1.bulk_add(multi)
+puts ""
 
 puts "bulk mlultiply"
 puts c1.bulk_mlultiply(multi)
+puts ""
 
+puts "STATUS"
 c1.get_stats()
 
 puts "end"
