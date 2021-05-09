6ccc567f3aa09444c1542b564cf4ea390f2cae18
abdelrahmany0 <abdelrhmanatta01@gmail.com>
2021-05-09 16:24:26 +0200

    adding Creat, Read, Update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,44 @@
 class ArticlesController < ApplicationController
-  def new; end
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
 
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(params.require(:article).permit(:title, :text))
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 end
