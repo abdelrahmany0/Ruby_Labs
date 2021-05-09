3185685053264e2965730a5156d48ff4ab831932
abdelrahmany0 <abdelrhmanatta01@gmail.com>
2021-05-09 17:05:24 +0200

    fixed edit & update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,10 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
     @article = Article.new
   end
@@ -17,28 +21,32 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
 
   def update
+    puts 'in update'
     @article = Article.find(params[:id])
-
+    puts 'before if'
     if @article.update(article_params)
-      redirect_to article_path(@article)
+      redirect_to articles_path
     else
+      puts 'false'
       render 'edit'
     end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
   end
 
   private
 
   def article_params
     params.require(:article).permit(:title, :text)
-  end
-
-  def edit
-    @article = Article.find(params[:id])
-  end
-
-  def show
-    @article = Article.find(params[:id])
   end
 end
